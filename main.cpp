3d142db96d3244bad8b06e003df315c551dd929e
Alisa Artemenko <alisa.artemenko2008@gmail.com>
2026-03-04 19:00:48 -0800

    Added main.cpp file

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include "character.h"
+#include "weapon.h"
+#include "enemy.h"
+
+using namespace std;
+
+int main() {
+    
+    Character hero("Aragorn");
+    Character defaultHero;
+    Character hero2("Silver", 500, "Beginner");
+    cout << "---" << endl;
+
+    Weapon stick("Stick", 10, 5, false);
+    Weapon sword("FireArm", 100);
+    cout << "---" << endl;
+
+    Enemy defaultEnemy;
+    Enemy dragon("Deathly Claw", 500, 1000);
+    cout << "---" << endl;
+
+    return 0;
+}
