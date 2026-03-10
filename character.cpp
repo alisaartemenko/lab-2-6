353824a82a010b15986d96443c3f048627b431e3
Alisa Artemenko <alisa.artemenko2008@gmail.com>
2026-03-09 18:59:48 -0700

    Implement copy and move constructors

---
+++ character.cpp
@@ -0,0 +1,38 @@
+#include "Character.h"
+#include <utility>
+
+using namespace std;
+
+Character::Character(const Character& other)
+    : name(other.name + " (Clone)"), health(other.health), ranking(other.ranking) {
+    cout << "Copy constructor called for " << name << endl;
+}
+
+Character::Character(Character&& other) noexcept
+    : name(move(other.name)), health(other.health), ranking(move(other.ranking)) {
+
+    other.health = 0;
+    other.ranking = "None";
+
+    cout << "Move constructor called. Original object is now empty." << endl;
+}
+
+Character::Character(string n, int h, string r) : name(n), health(h), ranking(r) {
+    cout << "Character " << name << " created" << endl;
+}
+
+Character::Character(string n) : Character(n, 100, "Beginner") {}
+
+Character::~Character() {
+    cout << "Character " << name << " deleted" << endl;
+}
+
+
+
+void Character::takeDamage(int damage) {
+    health -= damage;
+}
+
+void Character::showStats () {
+    cout << "Health: " << health << endl;
+}
---
+++ character.h
@@ -0,0 +1,29 @@
+#ifndef CHARACTER_H
+#define CHARACTER_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class Character {
+private:
+    string name;
+    int health;
+    string ranking;
+
+public:
+
+    Character(string n, int h, string r);
+    Character(string n = "Traveler");
+
+    Character(const Character& other);
+    Character(Character&& other) noexcept;
+
+    ~Character();
+
+    void takeDamage(int damage);
+    void showStats();
+};
+
+#endif
